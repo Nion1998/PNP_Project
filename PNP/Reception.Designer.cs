a57992378ecd83d3b3a93b99c71b349df766d207
Nion1998 <55551279+Nion1998@users.noreply.github.com>
2021-05-01 02:48:10 +0600

    Nion

--- PNP/Reception.Designer.cs
+++ PNP/Reception.Designer.cs
@@ -29,20 +29,97 @@ namespace PNP
         /// </summary>
         private void InitializeComponent()
         {
+            this.label3 = new System.Windows.Forms.Label();
+            this.login = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbpassword = new System.Windows.Forms.TextBox();
+            this.tbusername = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(341, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 25);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Login Form";
+            // 
+            // login
+            // 
+            this.login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login.Location = new System.Drawing.Point(465, 264);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(104, 38);
+            this.login.TabIndex = 12;
+            this.login.Text = "login";
+            this.login.UseVisualStyleBackColor = true;
+            this.login.Click += new System.EventHandler(this.login_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(213, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 29);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Password :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(197, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 29);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "User Name :";
+            // 
+            // tbpassword
+            // 
+            this.tbpassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbpassword.Location = new System.Drawing.Point(377, 192);
+            this.tbpassword.Name = "tbpassword";
+            this.tbpassword.Size = new System.Drawing.Size(192, 30);
+            this.tbpassword.TabIndex = 8;
+            // 
+            // tbusername
+            // 
+            this.tbusername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbusername.Location = new System.Drawing.Point(377, 132);
+            this.tbusername.Name = "tbusername";
+            this.tbusername.Size = new System.Drawing.Size(192, 30);
+            this.tbusername.TabIndex = 7;
             // 
             // Reception
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(813, 513);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbpassword);
+            this.Controls.Add(this.tbusername);
             this.Name = "Reception";
             this.Text = "Reception";
             this.Load += new System.EventHandler(this.Reception_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button login;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbpassword;
+        private System.Windows.Forms.TextBox tbusername;
     }
 }
--- PNP/Reception.cs
+++ PNP/Reception.cs
@@ -7,14 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace PNP
 {
+    
     public partial class Reception : Form
     {
+        SqlConnection con = new SqlConnection(@"Server=LAPTOP-0A0FEGEC\SQLEXPRESS; Database=PNP; User Id=nion; Password=nion;");
         public Reception()
         {
             InitializeComponent();
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("select * from R_tbl where user_name=@user_name and pass=@pass", con);
+            cmd.Parameters.AddWithValue("@user_name", tbusername.Text);
+            cmd.Parameters.AddWithValue("@pass", tbpassword.Text);
+            con.Open();
+            SqlDataAdapter adpt = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            adpt.Fill(ds);
+            con.Close();
+
+            int count = ds.Tables[0].Rows.Count;
+
+            if (count == 1)
+            {
+                MessageBox.Show("login");
+            }
+           else { MessageBox.Show("Error"); }
         }
 
         private void Reception_Load(object sender, EventArgs e)
