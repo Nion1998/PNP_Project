2cbb621118160d65396796f99d15c1d62a3b0b5c
Nion1998 <55551279+Nion1998@users.noreply.github.com>
2021-05-01 02:05:31 +0600

    Nion

---
+++ PNP/Reception.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PNP
+{
+    public partial class Reception : Form
+    {
+        public Reception()
+        {
+            InitializeComponent();
+        }
+
+        private void Reception_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
